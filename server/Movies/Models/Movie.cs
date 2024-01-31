cb2e29afb39b0fe1ca3ae973c49c84cced618f58
sadik312 <sadikislam46@gmail.com>
2024-01-31 20:05:46 +0000

    scaffolded movie pages

--- server/Movies/Models/Movie.cs
+++ server/Movies/Models/Movie.cs
@@ -6,7 +6,7 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; } // string? - denotes nullable type
-    [DataType(DateType.Date)]
+    //[DataType(DateType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
     public decimal Price { get; set; }
